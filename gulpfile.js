32d8b6d9f6e1e7c53ab05273b8a355e41a1baf9c
Suresh <vyassuresh108@gmail.com>
2018-04-01 00:23:47 +0530

    Emptied the master branch

--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-require('./gulp/tasks/styles');
-require('./gulp/tasks/watch');
-require('./gulp/tasks/sprites');
-require('./gulp/tasks/scripts');
-require('./gulp/tasks/build');
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-module.exports = {
-    entry: "./app/assets/scripts/app.js",
-    output: {
-        path: path.resolve(__dirname, "./app/temp/scripts"),
-        filename: 'app-bundled.js'
-    }
-}
